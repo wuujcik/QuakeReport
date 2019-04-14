738e528c236c5fe460dd92e91425f63fe7e63ffa
wuujcik <aleksandra.wojcikowska@tutanota.com>
2019-04-14 03:01:19 +0200

    Take the JSON from url and show current data in app
    The hardcoded JSON response is removed and added url downloading JSON from API.

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -15,21 +15,25 @@
  */
 package com.example.android.quakereport;
 
-import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
-
 import java.util.ArrayList;
+import java.util.List;
 
 public class EarthquakeActivity extends AppCompatActivity {
 
+    // Adapter for the list of earthquakes
+    private EarthquakeAdapter mAdapter;
+
+    //URL for earthquake data from the USGS dataset
+    private static final String USGS_REQUEST_URL =
+            "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=6&limit=10";
 
 
     @Override
@@ -37,28 +41,56 @@ public class EarthquakeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
 
-        //Get the list of earthquakes from {@link QueryUtils}
-        final ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes();
-
-
-        //sets the layout to use PlaceAdapter
-        EarthquakeAdapter adapter = new EarthquakeAdapter(EarthquakeActivity.this, earthquakes);
+        // Create a new adapter that takes an empty list of earthquakes as input
+        mAdapter = new EarthquakeAdapter(this, new ArrayList<Earthquake>());
 
         // Get a reference to the ListView, and attach the adapter to the listView.
-        ListView earthquakeListView = (ListView) findViewById(R.id.list);
-        earthquakeListView.setAdapter(adapter);
+        ListView earthquakeListView = findViewById(R.id.list);
+        earthquakeListView.setAdapter(mAdapter);
 
         //When a list item is clicked, the website with more details about the earthquake is opened
         earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapter, View view, int position, long id) {
-                Earthquake earthquake = earthquakes.get(position);
-                if (earthquake.getmWeb() != null) {
+                Earthquake currentEarthquake = mAdapter.getItem(position);
+
+                if (currentEarthquake.getmWeb() != null) {
                     Intent intent = new Intent(Intent.ACTION_VIEW);
-                    intent.setData(Uri.parse(earthquake.getmWeb()));
+                    intent.setData(Uri.parse(currentEarthquake.getmWeb()));
                     startActivity(intent);
                 }
             }
         });
+        EarthquakeAsyncTask task = new EarthquakeAsyncTask();
+        task.execute(USGS_REQUEST_URL);
     }
+    private class EarthquakeAsyncTask extends AsyncTask<String, Void, List<Earthquake>> {
+
+
+        @Override
+        protected List<Earthquake> doInBackground(String... urls) {
+                       // if there's no url or first url is null, return early with null
+            if (urls.length < 1 || urls[0] == null) {
+                return null;
+            } else {
+                // Perform the HTTP request for earthquake data and process the response.
+                List<Earthquake> result = QueryUtils.fetchEarthquakeData(urls[0]);
+                return result;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(List<Earthquake> data) {
+            // Clear the adapter of previous earthquake data
+            mAdapter.clear();
+
+            // Update the information displayed to the user if there was a result
+            if(data != null && !data.isEmpty()) {
+              mAdapter.addAll(data);
+            }
+        }
+
+    }
+
+
 }
